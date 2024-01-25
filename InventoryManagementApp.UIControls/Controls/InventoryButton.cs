19bb353c5a13d37f0fac3064278aa232b68ccf38
Jacob Bananal <jacob.bananal@tek84.com>
2024-01-25 14:07:39 -0800

    InventoryManagementApp.UIControls - Added custom InventoryButton and InventoryTextbox

---
+++ InventoryManagementApp.UIControls/Controls/InventoryButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InventoryManagementApp.UIControls
+{
+    public class InventoryButton : Button
+    {
+        static InventoryButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(InventoryButton), new FrameworkPropertyMetadata(typeof(InventoryButton)));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(InventoryButton), new PropertyMetadata(default(CornerRadius)));
+
+    }
+}
